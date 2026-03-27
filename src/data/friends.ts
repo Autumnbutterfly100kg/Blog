96e5c166c3fe7b5fe44d65995ef934f7ebf6028e
Autumnbutterfly100kg <1041673204@qq.com>
2026-03-28 01:47:29 +0800

    更新友链1头像图片

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -8,14 +8,14 @@ export interface FriendItem {
 }
 
 export const friendsData: FriendItem[] = [
-    {
-        "id": 1,
-        "title": "藍二乗的小站",
-        "imgurl": "https://s1.img-e.com/20260328/69c6b45b39aa5.jpg",
-        "desc": "藍二乗的博客",
-        "siteurl": "https://yuki2012720.github.io/",
-        "tags": []
-    }
+	{
+		id: 1,
+		title: "藍二乗的小站",
+		imgurl: "/images/friends/woshinidie.jpg",
+		desc: "藍二乗的博客",
+		siteurl: "https://yuki2012720.github.io/",
+		tags: [],
+	},
 ];
 
 // 获取所有友情链接数据
