47295b38877d9fbb87ce48724c8886e51e650e81
Autumnbutterfly100kg <1041673204@qq.com>
2026-03-28 19:20:56 +0800

    .

--- src/config.ts
+++ src/config.ts
@@ -166,7 +166,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "わたしの部屋", // 主页横幅主标题
+			title: "阿秋的小屋", // 主页横幅主标题
 
 			subtitle: [
 				"特別なことはないけど、君がいると十分です",
