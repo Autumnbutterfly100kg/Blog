4f07720a24c7ec98bbc5e9d7f368640692246081
Autumnbutterfly100kg <1041673204@qq.com>
2026-03-28 20:21:15 +0800

    .

--- src/components/widgets/music-player/constants.ts
+++ src/components/widgets/music-player/constants.ts
@@ -8,7 +8,7 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 1,
 		title: "AM",
-		artist: "Unknown",
+		artist: "えびかれー伯爵",
 		cover: "assets/music/cover/AM.webp",
 		url: "assets/music/url/AM.mp3",
 		duration: 0,
@@ -16,7 +16,7 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 2,
 		title: "Sneaky Driver",
-		artist: "Unknown",
+		artist: "Various Artists",
 		cover: "assets/music/cover/Sneaky Driver.webp",
 		url: "assets/music/url/Sneaky Driver.mp3",
 		duration: 0,
@@ -24,7 +24,7 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 3,
 		title: "この櫻ノ詩の下",
-		artist: "Unknown",
+		artist: "松本文紀",
 		cover: "assets/music/cover/この櫻ノ詩の下.webp",
 		url: "assets/music/url/この櫻ノ詩の下.mp3",
 		duration: 0,
@@ -32,7 +32,7 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 4,
 		title: "伸びた夕影",
-		artist: "Unknown",
+		artist: "藤井稿太郎,
 		cover: "assets/music/cover/伸びた夕影.webp",
 		url: "assets/music/url/伸びた夕影.mp3",
 		duration: 0,
@@ -40,23 +40,23 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 5,
 		title: "僕と花",
-		artist: "Unknown",
+		artist: "サカナクション",
 		cover: "assets/music/cover/僕と花.webp",
 		url: "assets/music/url/僕と花.mp3",
 		duration: 0,
 	},
 	{
 		id: 6,
-		title: "元気なランス R8",
-		artist: "Unknown",
-		cover: "assets/music/cover/元気なランス R8.webp",
-		url: "assets/music/url/元気なランス R8.mp3",
+		title: "元気なランスR8",
+		artist: "アリスソフト",
+		cover: "assets/music/cover/元気なランスR8.webp",
+		url: "assets/music/url/元気なランスR8.mp3",
 		duration: 0,
 	},
 	{
 		id: 7,
 		title: "夜の向日葵",
-		artist: "Unknown",
+		artist: "松本文紀",
 		cover: "assets/music/cover/夜の向日葵.webp",
 		url: "assets/music/url/夜の向日葵.mp3",
 		duration: 0,
@@ -64,7 +64,7 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 8,
 		title: "夢の歩みを見上げて",
-		artist: "Unknown",
+		artist: "松本文紀",
 		cover: "assets/music/cover/夢の歩みを見上げて.webp",
 		url: "assets/music/url/夢の歩みを見上げて.mp3",
 		duration: 0,
@@ -72,7 +72,7 @@ export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 9,
 		title: "思考",
-		artist: "Unknown",
+		artist: "未来",
 		cover: "assets/music/cover/思考.webp",
 		url: "assets/music/url/思考.mp3",
 		duration: 0,
